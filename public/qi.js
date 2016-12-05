e241354a9c73cafd9fdf0145a5a7f361a2152be1
Junran Xu <trevorjr118@gmail.com>
2016-12-05 01:46:25 -0500

    Update qi.js

--- public/qi.js
+++ public/qi.js
@@ -1651,7 +1651,7 @@ function win() {
 				var _i = this.id.slice(0,pos);
 				var _j = this.id.slice(pos+1,this.id.length);
 				//alert(_i+":"+_j);
-				alert(localStorage.getItem('pl'));
+				//alert(localStorage.getItem('pl'));
 				
 				if (localStorage.getItem('pl') == 0) {
 					
