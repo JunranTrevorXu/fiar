6b1c2ad6b7fdf45a60f91ce763edadc54380caf2
Junran Xu <trevorjr118@gmail.com>
2016-12-05 02:16:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,24 +7,74 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
-var MongoClient = require('mongodb').MongoClient;
 //var assert = require('assert');
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
+
+
+
+var MongoClient = require('mongodb').MongoClient;
 // create a new express server
 var app = express();
 
+
+var pvp = 0;
+var pve = 0;
+var url = 'mongodb://admin:GUBDQEQPNICZVOET@sl-us-dal-9-portal.3.dblayer.com:17399,sl-us-dal-9-portal.0.dblayer.com:17399/admin?ssl=true';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
+
 app.get('/pvp', function(req, res) {
 	
-	console.log("receivepvp");
+	pvp++;
+	MongoClient.connect(url, function(err, db) {
+  	assert.equal(null, err);
+	  insertDocumentpvp(db, function() {
+	      db.close();
+	  });
+});
 });
 
 app.get('/pve', function(req, res) {
 	
-	console.log("receivepve");
+	pve++;
+	MongoClient.connect(url, function(err, db) {
+  	assert.equal(null, err);
+  	insertDocumentpve(db, function() {
+      db.close();
+  });
 });
+});
+
+var insertDocumentpvp = function(db, callback) {
+   db.collection('fiar').insertOne( {
+      "optionpvp":pvp
+   }, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted a document into the restaurants collection.");
+    callback();
+  });
+};
+
+var insertDocumentpve = function(db, callback) {
+   db.collection('fiar').insertOne( {
+      "optionpve":pve
+   }, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted a document into the restaurants collection.");
+    callback();
+  });
+};
+
+
+
+
+
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
